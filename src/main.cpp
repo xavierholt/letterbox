7ca5d946af42519b6b5f27050b0a587ac4682b62
Xavier Holt <xavierholt@gmail.com>
2013-06-14 01:21:08 +0100

    Readme text.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
+/******************************************************************************
+    This file is part of LetterBox.
+
+  LetterBox is free software: you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free
+  Software Foundation, either version 3 of the License, or (at your option)
+  any later version.
+  
+  LetterBox is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+  more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with LetterBox.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #include "window.h"
 
 #include <QtCore/QFile>
@@ -8,7 +25,7 @@ int main(int argc, char* argv[])
 	QApplication app(argc, argv);
 	
 	Window win;
-	QFile style("style.qtss");
+	QFile style("style.css");
 	style.open(QIODevice::ReadOnly);
 	win.setStyleSheet(style.readAll());
 	win.show();
--- src/moc_button.cpp
+++ src/moc_button.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'button.h'
 **
-** Created: Thu Jun 13 21:35:58 2013
+** Created: Fri Jun 14 00:54:02 2013
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.3)
 **
 ** WARNING! All changes made in this file will be lost!
